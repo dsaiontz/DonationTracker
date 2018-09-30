5566221b05445dbe803ab2d1c1f0d142a0f0bc3e
David <david.saiontz@gmail.com>
2018-09-30 12:03:35 -0400

    Add credentials class

---
+++ app/src/main/java/com/example/david/donationtracker/Credentials.java
@@ -0,0 +1,10 @@
+package com.example.david.donationtracker;
+
+import java.util.HashMap;
+
+public class Credentials {
+
+    private HashMap<String, User> users;
+    private HashMap<String, User> admins;
+    private HashMap<String, User> employees;
+}
