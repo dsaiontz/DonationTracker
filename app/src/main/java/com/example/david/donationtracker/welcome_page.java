84c72e5a76696713069d25fa1faee083d3a843ce
Spencer <sscheckmate1@gmail.com>
2018-09-30 11:06:54 -0400

    Update From Master

---
+++ app/src/main/java/com/example/david/donationtracker/welcome_page.java
@@ -0,0 +1,28 @@
+package com.example.david.donationtracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class welcome_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+        Button LoginButton = (Button) findViewById(R.id.LogInButtonThing);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+        startActivity(new Intent(welcome_page.this, LoginActivity.class));
+        finish();
+    }
+}
