ccf62d29f06bb3ec7fafa94fa940608ae59ec1c7
David <david.saiontz@gmail.com>
2018-10-01 14:06:03 -0400

    fixing merge issues

--- app/src/main/java/com/example/david/donationtracker/welcome_page.java
+++ app/src/main/java/com/example/david/donationtracker/welcome_page.java
@@ -12,19 +12,13 @@ public class welcome_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
-<<<<<<< HEAD
-        Button LoginButton = (Button) findViewById(R.id.LogInButtonThing);
-=======
         Button LoginButton = (Button) findViewById(R.id.WelcomePageLoginButton);
->>>>>>> Spencer
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 logout();
             }
         });
-<<<<<<< HEAD
-=======
         Button RegisterButton = (Button) findViewById(R.id.WelcomePageRegisterButton);
         RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,19 +26,15 @@ public class welcome_page extends AppCompatActivity {
                 register();
             }
         });
->>>>>>> Spencer
     }
 
     private void logout() {
         startActivity(new Intent(welcome_page.this, LoginActivity.class));
         finish();
     }
-<<<<<<< HEAD
-=======
 
     private void register() {
         startActivity(new Intent(welcome_page.this, RegisterActivity.class));
         finish();
     }
->>>>>>> Spencer
 }
