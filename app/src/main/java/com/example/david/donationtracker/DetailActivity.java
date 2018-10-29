266491affc45440e5e0b89284ad299038f381112
Spencer <sscheckmate1@gmail.com>
2018-10-29 13:12:52 -0400

    Update app/src/main/java/com/example/david/donationtracker/DetailActivity.java

--- app/src/main/java/com/example/david/donationtracker/DetailActivity.java
+++ app/src/main/java/com/example/david/donationtracker/DetailActivity.java
@@ -1,14 +1,48 @@
+package com.example.david.donationtracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private String username;
+    private String locationName;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView locationRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent grabbedIntent = getIntent();
+        Bundle bundle = grabbedIntent.getExtras();
+        username = bundle.getString("username");
+        locationName = bundle.getString("locationName");
 
         Locations locations = new Locations();
 
         final Location location = Locations.get(locationName);
         final User user = Credentials.get(username);
 
+        // configures the recycler view
         adapter = new DonationAdapter(Donations.getDonations(locations.get(locationName)), null, username);
         locationRecyclerView = findViewById(R.id.locationRecyclerView);
         locationRecyclerView.setHasFixedSize(true);
         locationRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         locationRecyclerView.setAdapter(adapter);
+
         TextView textView = findViewById(R.id.detailText);
         if (location != null) {
             String detailText = "Name: " + location.getName();
@@ -16,6 +50,9 @@
                     + "\nLongitude: " + location.getLongitude() + "\nLatitude: "
                     + location.getLatitude() + "\nAddress: " + location.getAddress()
                     + "\nPhone Number: " + location.getPhoneNumber() + "\n";
+            textView.setText(detailText);
+        }
+
         Button donationButton = findViewById(R.id.donationButton);
         donationButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,3 +76,18 @@
                 }
             }
         });
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent grabbedIntent = getIntent();
+                Intent toLocationActivity = new Intent(DetailActivity.this, LocationActivity.class);
+                toLocationActivity.putExtra("username", grabbedIntent.getExtras().getString("username"));
+                startActivity(toLocationActivity);
+                finish();
+            }
+        });
+    }
+
+}
