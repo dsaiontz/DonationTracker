35f95fa7b76fc67d08f06cb48abca8300f00de86
Jackson Huffman <jhuffman134@gmail.com>
2018-10-29 19:08:24 -0400

    can now click on donations

--- app/src/main/java/com/example/david/donationtracker/DonationDetailActivity.java
+++ app/src/main/java/com/example/david/donationtracker/DonationDetailActivity.java
@@ -3,6 +3,7 @@ package com.example.david.donationtracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class DonationDetailActivity extends AppCompatActivity {
@@ -12,15 +13,17 @@ public class DonationDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation_detail_layout);
 
+        Log.e("","inside donationdetailactivity");
+
         Intent intent = getIntent();
 
-        Donation donation = (Donation) intent.getSerializableExtra("donation");
+        //Donation donation = (Donation) intent.getSerializableExtra("donation");
 //        Donation donation = new Donation(intent.getStringExtra("donation"),
 //                Locations.get(intent.getStringExtra("location")), intent.getStringExtra("short description"),
 //                intent.getStringExtra("full description"), Double.parseDouble(intent.getStringExtra("value")),
 //                DonationCategory.valueOf(intent.getStringExtra("category")));
 
         TextView textView = (TextView) findViewById(R.id.donation);
-        textView.setText(donation.toString());
+        textView.setText(Donations.getCurrentDonation().toString());
     }
 }
