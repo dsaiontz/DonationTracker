b1d6607a9d3f1f4927d703b7e0482242c2dd6424
Spencer <sscheckmate1@gmail.com>
2018-10-29 10:36:46 -0400

    Update app/src/main/java/com/example/david/donationtracker/DetailActivity.java

--- app/src/main/java/com/example/david/donationtracker/DetailActivity.java
+++ app/src/main/java/com/example/david/donationtracker/DetailActivity.java
@@ -1,28 +1,18 @@
 package com.example.david.donationtracker;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.time.LocalDateTime;
-
-import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -44,9 +34,6 @@ public class DetailActivity extends AppCompatActivity {
         final Location location = Locations.get(locationName);
         final User user = Credentials.get(username);
 
-        String donationsText = "";
-        ArrayList<Donation> donationsForLocation = Donations.getDonations(location);
-
         // configures the recycler view
         adapter = new DonationAdapter(Donations.getDonations(Locations.get(locationName)), null, username);
         locationRecyclerView = findViewById(R.id.locationRecyclerView);
@@ -54,18 +41,17 @@ public class DetailActivity extends AppCompatActivity {
         locationRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         locationRecyclerView.setAdapter(adapter);
 
-        TextView textView = (TextView) findViewById(R.id.detailText);
+        TextView textView = findViewById(R.id.detailText);
         if (location != null) {
             String detailText = "Name: " + location.getName();
-            Log.i("", "NOT WORKING");
             detailText = detailText + "\nType: " + location.getType()
                     + "\nLongitude: " + location.getLongitude() + "\nLatitude: "
                     + location.getLatitude() + "\nAddress: " + location.getAddress()
-                    + "\nPhone Number: " + location.getPhoneNumber() + "\n" + donationsText;
+                    + "\nPhone Number: " + location.getPhoneNumber() + "\n";
             textView.setText(detailText);
         }
 
-        Button donationButton = (Button) findViewById(R.id.donationButton);
+        Button donationButton = findViewById(R.id.donationButton);
         donationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,7 +60,6 @@ public class DetailActivity extends AppCompatActivity {
                 if ((user.getUserType() == UserType.EMPLOYEE) ||
                         (user.getUserType() == UserType.ADMIN) ||
                         (user.getUserType() == UserType.MANAGER)) {
-                    Log.e("","if runs");
                     Intent intent = new Intent(DetailActivity.this, DonationActivity.class);
                     intent.putExtra("location", location);
                     intent.putExtra("username", username);
@@ -92,7 +77,7 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
-        Button backButton = (Button) findViewById(R.id.backButton);
+        Button backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
