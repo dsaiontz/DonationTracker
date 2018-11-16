f650b51b0ec5b9ddde95b9afc1b16ad5e4b539a1
James Crowley <james.ryan.crowley@gmail.com>
2018-11-16 04:23:39 -0500

    Update app/src/test/java/com/example/david/donationtracker/JamesUnitTest.java

---
+++ app/src/test/java/com/example/david/donationtracker/JamesUnitTest.java
@@ -0,0 +1,28 @@
+package com.example.david.donationtracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class JamesUnitTest {
+
+    @Test
+    public void validateUsernameIsCorrect() {
+
+        ArrayList<Location> locs = Locations.getAllLocations();
+        ArrayList<Location> notLocs = new ArrayList<>();
+        notLocs.add(new Location("notALoc1"));
+        notLocs.add(new Location("notALoc2"));
+        notLocs.add(new Location("notALoc3"));
+
+        for (Location s : locs) {
+            System.out.println(s);
+            assert(Locations.containsLocation(s.getName()));
+        }
+
+        for (Location s : notLocs) {
+            System.out.println(s);
+            assert(!Locations.containsLocation(s.getName()));
+        }
+    }
+}
