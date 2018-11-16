0fe4d535a5249435979d86087db42b4423a1b96b
Jackson Huffman <jhuffman134@gmail.com>
2018-11-16 11:29:12 -0500

    JUnit done

---
+++ app/src/test/java/com/example/david/donationtracker/JacksonUnitTest.java
@@ -0,0 +1,34 @@
+package com.example.david.donationtracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JacksonUnitTest {
+
+    @Test
+    public void validateAddLocation() {
+
+        List<Location> newLocations = new ArrayList<>();
+
+        newLocations.add(new Location("Location 1"));
+        newLocations.add(new Location("Location 2"));
+        newLocations.add(new Location("Location 3"));
+        newLocations.add(new Location("Location 4"));
+
+
+        for (Location loc : newLocations) {
+            //System.out.println(s);
+            assert(!Locations.getAllLocations().contains(loc));
+            Locations.add(loc);
+        }
+
+
+
+        for (Location loc : newLocations) {
+            //System.out.println(s);
+            assert(Locations.getAllLocations().contains(loc));
+        }
+    }
+}
